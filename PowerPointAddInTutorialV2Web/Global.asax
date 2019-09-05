7f9ccd0963dc2cba2a5caa2e43cdc1fb665dbe0f
sonegishi <5055ngs@gmail.com>
2019-09-04 22:36:01 -0400

    Enable users to add an image from Bing

---
+++ PowerPointAddInTutorialV2Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PowerPointAddInTutorialV2Web.WebApiApplication" Language="C#" %>
--- PowerPointAddInTutorialV2Web/Home.js
+++ PowerPointAddInTutorialV2Web/Home.js
@@ -1,10 +1,8 @@
-﻿
-(function () {
+﻿(function () {
     "use strict";
 
     var messageBanner;
 
-    // The initialize function must be run each time a new page is loaded.
     Office.initialize = function (reason) {
         $(document).ready(function () {
             // Initialize the FabricUI notification mechanism and hide it
@@ -12,22 +10,44 @@
             messageBanner = new fabric.MessageBanner(element);
             messageBanner.hideBanner();
 
-            $('#get-data-from-selection').click(getDataFromSelection);
+            // TODO1: Assign event handler for insert-image button.
+            $('#insert-image').click(insertImage);
+            // TODO4: Assign event handler for insert-text button.
+            // TODO6: Assign event handler for get-slide-metadata button.
+            // TODO8: Assign event handlers for the four navigation buttons.
         });
     };
 
-    // Reads data from current document selection and displays a notification
-    function getDataFromSelection() {
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-            function (result) {
-                if (result.status === Office.AsyncResultStatus.Succeeded) {
-                    showNotification('The selected text is:', '"' + result.value + '"');
-                } else {
-                    showNotification('Error:', result.error.message);
-                }
+    // TODO2: Define the insertImage function.
+    function insertImage() {
+        // Get image from from web service (as a Base64 encoded string).
+        $.ajax({
+            url: "/api/Photo/", success: function (result) {
+                insertImageFromBase64String(result);
+            }, error: function (xhr, status, error) {
+                showNotification("Error", "Oops, something went wrong.");
             }
-        );
+        });
     }
+
+    // TODO3: Define the insertImageFromBase64String function.
+    function insertImageFromBase64String(image) {
+        // Call Office.js to insert the image into the document.
+        Office.context.document.setSelectedDataAsync(image, {
+            coercionType: Office.CoercionType.Image
+        },
+            function (asyncResult) {
+                if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+                    showNotification("Error", asyncResult.error.message);
+                }
+            });
+    }
+
+    // TODO5: Define the insertText function.
+
+    // TODO7: Define the getSlideMetadata function.
+
+    // TODO9: Define the navigation functions.
 
     // Helper function for displaying notifications
     function showNotification(header, content) {
